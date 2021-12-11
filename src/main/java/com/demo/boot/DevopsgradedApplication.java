c8e2eaec4c00fae26b4c263312f2cc73354807ad
santosh02k <95336552+santosh02k@users.noreply.github.com>
2021-12-11 17:48:27 +0530

    Add files via upload

---
+++ src/main/java/com/demo/boot/DevopsgradedApplication.java
@@ -0,0 +1,14 @@
+package com.demo.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsgradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsgradedApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
